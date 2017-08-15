3979391f3446713839d62715b28dcdaf71064460
mydoum <a.alonso.giraldo@gmail.com>
2017-08-16 01:40:08 +0200

    Adding the configuration for spark

---
+++ src/main/scala/com/earlybirds/challenge/configuration/DataProcessorConfiguration.scala
@@ -0,0 +1,33 @@
+package com.earlybirds.challenge.configuration
+
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.types.{DataTypes, StructField, StructType}
+/**
+  * Created by alonso on 8/16/17.
+  */
+class DataProcessorConfiguration {
+    private val user_id     = StructField("user_id", DataTypes.StringType)
+    private val item_id     = StructField("item_id", DataTypes.StringType)
+    private val rating      = StructField("rating",  DataTypes.FloatType)
+    private val timestamp   = StructField("type",    DataTypes.TimestampType)
+
+    private val fields = Array(user_id, item_id, rating, timestamp)
+
+    val dataSchema = StructType(fields)
+
+    object SparkJob {
+      private val sqlContext = SparkSession
+        .builder
+        .appName("Anime rating")
+        .master("local[*]")
+        .getOrCreate()
+
+      sqlContext.sparkContext.setLogLevel("ERROR")
+
+      def getSqlContext() = {
+        sqlContext
+      }
+    }
+
+  }
+}
